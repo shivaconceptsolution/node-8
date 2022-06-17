8d168437214ff269744b675d995dade7487f1887
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2022-06-17 10:08:15 +0530

    Add files via upload

--- studentmongo.js
+++ studentmongo.js
@@ -4,42 +4,68 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017";
 var s = "";
 http.createServer(function(req,res){
+if(req.url!='/favicon.ico')
+{
 
  var q = obj.parse(req.url, true);
+ let db1 = null;
+ let db2 = null;
 var qdata = q.query;
 var r = qdata.txt1;
 var n = qdata.txt2;
 var b = qdata.txt3;
 var f = qdata.txt4;
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
+insertdb();
+//getdb();
+
+
+ function insertdb() {
+   MongoClient.connect(url, function(err, db) {
     var dbo = db.db("scsdb");
+    if (err) throw err;
+    
     dbo.collection("student").insertOne({rno:r,name:n,branch:b,fees:f},function(err,res){
         if (err) throw err;
         console.log("Student Record Addedd Successfully");
-      
-    });
-    dbo.collection("student").find({}).toArray(function(err,res1){
-        if (err) throw err;
-        console.log(res1);
-     
-      s = s+"<table><tr><th>Rno</th><th>Name</th><th>Branch</th><th>Fees</th></tr>";
-        for(var i=0;i<res1.length;i++)
-        {
-           s = s + "<tr><td>"+res1[i].rno + "</td><td>"+ res1[i].name + "</td><td>"+res1[i].branch + "</td><td>" + res1[i].fees + "</td></tr>";
-        }
-        s = s + "</table>";
-        
-       // s = "<table border='1'><tr><th>Hello</th></table>"; 
-        console.log(s);
         db.close();
+        getdb();
     });
+  
+  });
+}
+ function getdb() {
+
+MongoClient.connect(url, function(err, db) {
+   
+    var dbo = db.db("scsdb");
+    dbo.collection("student").find({}).toArray(function(err,res1){
+      if (err) throw err;
+      console.log(res1);
+    s="";
+    s = s+"<table><tr><th>Rno</th><th>Name</th><th>Branch</th><th>Fees</th></tr>";
+      for(var i=0;i<res1.length;i++)
+      {
+         s = s + "<tr><td>"+res1[i].rno + "</td><td>"+ res1[i].name + "</td><td>"+res1[i].branch + "</td><td>" + res1[i].fees + "</td></tr>";
+      }
+      s = s + "</table>";
+      
+     // s = "<table border='1'><tr><th>Hello</th></table>"; 
+    
+      db.close();
+  });
    
   });
- 
+  setTimeout(function(){
+    res.write(s);
+    res.end();
+  },1000);
+  
+}
+  
+}
 
-res.write(s);
-res.end();
+
+
 
 }).listen('8089');
 
